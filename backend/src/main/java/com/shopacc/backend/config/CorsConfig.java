f05b9198d3c6096e7a7269e8d378cf853d62e0b7
ThongThien <thongthien2004@gmail.com>
2026-06-26 11:24:18 +0700

    feat(cors): add https://shopthien.xyz in CorsConfig

--- backend/src/main/java/com/shopacc/backend/config/CorsConfig.java
+++ backend/src/main/java/com/shopacc/backend/config/CorsConfig.java
@@ -18,9 +18,10 @@ public class CorsConfig {
 
                 CorsConfiguration config = new CorsConfiguration();
 
-                config.setAllowedOrigins(
-                                List.of(
-                                                "http://localhost:3000"));
+                config.setAllowedOrigins(List.of(
+                                "http://localhost:3000",
+                                "https://shopthien.xyz",
+                                "https://www.shopthien.xyz"));
 
                 config.setAllowedMethods(
                                 List.of(
